b95c6d051f7f84ab9009e86751ac0a4a7785486f
nathan <nathan@appflowy.io>
2023-02-06 15:30:47 +0800

    chore: migrate from appflowy main repo

---
+++ lib/appflowy_board.dart
@@ -0,0 +1,7 @@
+/// AppFlowyBoard library
+library appflowy_board;
+
+export 'src/widgets/board_group/group_data.dart';
+export 'src/widgets/board_data.dart';
+export 'src/widgets/styled_widgets/widgets.dart';
+export 'src/widgets/board.dart';
